07010c6e192827d8a321c6bd143f6907c0fb4797
vb@vvbb.fr <vb@vvbb.fr>
2021-10-20 01:11:23 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+import "./style.css";
+import "./AGInput.scss";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+
+document.querySelector("#app").innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`;
